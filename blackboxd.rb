cbd597cb5c4bb4fbd45a83b6392a421a131e549d
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-22 19:35:32 -0400

    Brew formula update for blackboxd version 0.2.0-rc5

--- blackboxd.rb
+++ blackboxd.rb
@@ -2,14 +2,14 @@
 class Blackboxd < Formula
   desc "A pluggable platform for multi-chain deployments."
   homepage "https://crypdex.io/"
-  version "0.2.0-rc4"
+  version "0.2.0-rc5"
 
   if OS.mac?
-    url "https://github.com/crypdex/blackbox/releases/download/0.2.0-rc4/blackboxd_0.2.0-rc4_darwin_amd64.tar.gz"
-    sha256 "fa00131d1cd3235c31eba68c7adb1117e3fb88b0124ea930fdb2a6bc8d785c23"
+    url "https://github.com/crypdex/blackbox/releases/download/0.2.0-rc5/blackboxd_0.2.0-rc5_darwin_amd64.tar.gz"
+    sha256 "7816674b951600c0ecebb01eacd76acf74a53b10acfe793569466937785dd4e5"
   elsif OS.linux?
-    url "https://github.com/crypdex/blackbox/releases/download/0.2.0-rc4/blackboxd_0.2.0-rc4_linux_amd64.tar.gz"
-    sha256 "ca02230074956058c18f8e41747f36affbeb3bc9bcb44bb3432ab7de5bd5e73c"
+    url "https://github.com/crypdex/blackbox/releases/download/0.2.0-rc5/blackboxd_0.2.0-rc5_linux_amd64.tar.gz"
+    sha256 "76ec95defbc388fa9be1f56748b07bece51329812c73f86bad2099c1194cf892"
   end
 
   def install
