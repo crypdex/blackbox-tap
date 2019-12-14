40978e8b6fd498ebc9431953d85a9e0112f39b49
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-14 09:36:36 -0500

    Brew formula update for blackbox-os version 0.2.32

--- blackbox-os.rb
+++ blackbox-os.rb
@@ -2,21 +2,21 @@
 class BlackboxOs < Formula
   desc "A pluggable platform for multi-chain deployments."
   homepage "https://crypdex.io/"
-  version "0.2.31"
+  version "0.2.32"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/crypdex/blackbox/releases/download/0.2.31/blackbox-os_0.2.31_darwin_amd64.tar.gz"
-    sha256 "1a42988e4adf731397eaf4cad050289b0fa49096d623ba36c17e715716c6a496"
+    url "https://github.com/crypdex/blackbox/releases/download/0.2.32/blackbox-os_0.2.32_darwin_amd64.tar.gz"
+    sha256 "e0be538dad81ab5a36b248075daa89ed8ef6934c336e67fd23b0e4686ade913c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/crypdex/blackbox/releases/download/0.2.31/blackbox-os_0.2.31_linux_amd64.tar.gz"
-      sha256 "bc5bcd97a96f3aea38eecac2b987e6611d5fd69388d37e81560bf4067f86565f"
+      url "https://github.com/crypdex/blackbox/releases/download/0.2.32/blackbox-os_0.2.32_linux_amd64.tar.gz"
+      sha256 "8db185ffa3dc92a7acd46abc24f7f333053395e3782e96ed96660e026c26eae2"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/crypdex/blackbox/releases/download/0.2.31/blackbox-os_0.2.31_linux_arm64.tar.gz"
-        sha256 "f239d2dff1aab4657913a7e463da9e171b771576bf9c5633329d98c24293993f"
+        url "https://github.com/crypdex/blackbox/releases/download/0.2.32/blackbox-os_0.2.32_linux_arm64.tar.gz"
+        sha256 "9425631ee008821fc0cce4f66df34d8ce632645e84153bce48e6ac8725044099"
       else
       end
     end
